249ddff411dd8a0d0c0e72194a75d1f74c7f4e85
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-07 09:37:56 +0100

    sort commit

---
+++ sort.js
@@ -0,0 +1,11 @@
+let name = ['jawad', 'Ayodele', 'Mide', 'Teslim']
+let namenums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let name2 = ['olamide', 'josh', 'Victor', 'Tope', 'Effectual']
+
+// let sorted1 = name.sort()
+// console.log(sorted1);
+
+console.table(namenums.sort())
+let nums = [4,5,1]
+console.log(nums.sort());
