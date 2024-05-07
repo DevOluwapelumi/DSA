b62ddb95d6da7ef1a43317e1989bb3869fbbf1ce
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-05-07 09:20:20 +0100

    queue commit

--- queue.js
+++ queue.js
@@ -1,17 +1,34 @@
-class queue {
-    constructor () {
-        this.all = []
+class Queue {
+    constructor() {
+        this.all = [];
+        this.size = this.all.length
     }
     enqueue(student) {
-        this.all.push(student)
+        this.all.push(student);
     }
     dequeue() {
-        this.all.shift()
+        if (!this.isEmpty()) {
+            return this.all.shift();
+        } else {
+            return "Queue is empty";
+        }
+    }
+    isEmpty() {
+        return this.all.length === 0;
+    }
+    front() {
+        return this.all[0];
+}
+    size() {
+        return this.all.length;
     }
 }
 
-let q1 = new Queue()
-q1.enqueue('Jawad')
-q1.enqueue('Jawad')
-q1.enqueue('Jawad')
-q1.dequeue()
+let q1 = new Queue();
+q1.enqueue('Pelumi');
+q1.enqueue('Jawad');
+q1.enqueue('Esther');
+q1.dequeue();
+q1.isEmpty();
+q1.front();
+console.log(q1);
